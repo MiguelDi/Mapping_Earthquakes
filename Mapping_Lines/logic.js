7d66077208d5892632f9ac328ec09f3fac1551d6
Miguel Diaz <migueldiaz@Miguels-MBP.attlocal.net>
2021-01-03 17:38:54 -0800

    finished on 13.4.3

--- Mapping_Lines/logic.js
+++ Mapping_Lines/logic.js
@@ -3,27 +3,20 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid', {
-    center: [34.0522, -118.2437], 
-    zoom: 14
-});
+let map = L.map('mapid').setView ([37.6213, -122.3790], 5)
 
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
 
-
-// `Import` cities object into our script as cityData
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        color: "#ffa500",
-        fillColor: "#ffa400",
-        fillOpacity: 0.2 //<- default setting
- }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> </hr> <h3>Population " + city.population.toLocaleString() + "<h3>").addTo(map);
-
-   });
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 
 
@@ -36,7 +29,7 @@ cityData.forEach(function(city) {
 //  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
